bca1b0a8ca2d0229b82fe50fd8d337a412badc18
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-09-12 08:41:03 -0700

    Update to use useRef

--- src/components/GeneralForm.jsx
+++ src/components/GeneralForm.jsx
@@ -1,24 +1,43 @@
 import '../styles/general-form.css'
+import { useRef } from 'react'
 
 export function GeneralForm() {
+    const firstName = useRef('')
+    const lastName = useRef('')
+    const email = useRef('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e)
+    }
 
     return (
         <>
         <h2>General Form</h2>
-        <div className="form">
+        <form onSubmit={handleSubmit} className="form">
             <div>
                 <label>First Name:</label>
-                <input type='text'></input>
+                <input
+                 type='text'
+                 onChange={(e) => (firstName.current = e.target.value)}
+                 />
             </div>
             <div>
                 <label>Last Name:</label>
-                <input type='text'></input>
+                <input
+                    type='text'
+                    onChange={(e) => (lastName.current = e.target.value)}
+                />
             </div>
             <div>
                 <label>Email:</label>
-                <input type='email'></input>
+                <input
+                    type='text'
+                    onChange={(e) => (email.current = e.target.value)}
+                />
             </div>
-        </div>
+            <button type="submit">Submit</button>
+        </form>
         </>
     )
 }
