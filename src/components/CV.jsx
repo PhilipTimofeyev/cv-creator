7438762dc8f3bbab2961206fcfb4cb777454f79e
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-09-12 16:33:48 -0700

    Update contact styling

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -8,11 +8,14 @@ export function CV({
     <div className="cv-container">
         <h1>{firstName} {lastName}</h1>
         <div className="general">
+            <h2>Contact</h2>
+            <h4>Email</h4>
             <p>{email}</p>
+            <h4>Phone</h4>
             <p>{phone}</p>
         </div>
         <div className="education">
-            <h1>Education</h1>
+            <h2>Education</h2>
             <h4>School Name</h4>
             <p>{schoolName}</p>
             <h4>Title of Study</h4>
@@ -21,17 +24,15 @@ export function CV({
             <p>{dateOfStudy}</p>
         </div>
         <div className="experience">
-            <h1>Experience</h1>
+            <h2>Experience</h2>
             <h4>Company</h4>
             <p>{company}</p>
             <h4>Position</h4>
             <p>{position}</p>
             <h4>Responsibilities</h4>
             <p>{responsibilities}</p>
-            <h4>Date From</h4>
-            <p>{dateFrom}</p>
-            <h4>Date To</h4>
-            <p>{dateTo}</p>
+            <h4>Dates</h4>
+            <p>{dateFrom} - {dateTo}</p>
         </div>
     </div>
     )
